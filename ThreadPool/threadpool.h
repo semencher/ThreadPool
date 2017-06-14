71c8b6938c9adc7ba3d35780dddf50f5ef602780
Semen <semen.cher@mail.ru>
2017-06-14 21:42:02 +0700

    Написал класс для очереди

--- ThreadPool/threadpool.h
+++ ThreadPool/threadpool.h
@@ -5,4 +5,41 @@
 
 */
 
+#include <mutex>
+#include <queue>
 
+namespace pt
+{
+	namespace squeue
+	{
+		template<typename T> class SafeQueue
+		{
+		public:
+			void push(const T &val) {
+				std::lock_guard<std::mutex> lock(mutexQueue);
+				queue.push(val);
+			}
+			bool pop(T &val) {
+				std::lock_guard<std::mutex> lock(mutexQueue);
+				if (queue.empty) {
+					return false;
+				}
+				val = queue.front();
+				queue.pop();
+				return true;
+			}
+			bool empty() {
+				std::lock_guard<std::mutex> lock(mutexQueue);
+				return queue.empty();
+			}
+		private:
+			std::queue<T> queue;
+			std::mutex mutexQueue;
+		};
+	}
+
+	template<typename T> class ThreadPool
+	{
+
+	};
+}
